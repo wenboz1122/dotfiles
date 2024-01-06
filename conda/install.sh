04cafa4adcd5cfb917d41947faec18203bd85862
Wenbo Zhang <wenboz1122@gmail.com>
2024-01-06 01:53:14 -0800

    add conda

---
+++ conda/install.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+~/miniconda3/bin/conda init zsh
