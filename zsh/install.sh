e9e4d4757f7fe5f5283f1f184794cc61f71533e7
wenboz1122 <101364632+wenboz1122@users.noreply.github.com>
2024-03-07 00:22:54 -0800

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,10 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install powerline fonts
-git clone https://github.com/powerline/fonts.git --depth=1 ~/repos/powerline
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
 ./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
