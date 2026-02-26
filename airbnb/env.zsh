b2c53f92aff5c1dcdf74e23625840607d6445312
Wenbo Zhang <wbz@meta.com>
2026-02-26 15:57:32 -0800

    byebye abnb

--- airbnb/env.zsh
+++
@@ -1,8 +0,0 @@
-export ONETOUCHGEN_LDAP_USER="w_zhang"
-export ONETOUCHGEN_ACCEPT_EULA=y
-export DATA_DIR=/Users/conor_camp/airbnb/data
-
-export DATA_DIR="$HOME/airlab/repos/data"
-export MINERVA_HOME="$HOME/airlab/repos/minerva"
-export MEASURED_HOME="$HOME/airlab/repos/measured"
-export SQLGLOT_HOME="$HOME/my_repos/sqlglot"
